d9820eb023e10a979c3752674761d01ce7aab1ce
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2021-01-25 21:56:57 -0500

    working on Pawn tutorial

--- Ryddelmyst/Source/Ryddelmyst/Public/MollyPawn.h
+++ Ryddelmyst/Source/Ryddelmyst/Public/MollyPawn.h
@@ -26,6 +26,7 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	
-	
+public:
+	UPROPERTY(EditAnywhere)
+	USceneComponent* VisibleMollyComponent;
 };
