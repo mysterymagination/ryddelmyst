d84e8e2b198c44aa3cf0cdd20b0f72ca50b0c954
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2021-01-25 21:56:57 -0500

    working on Pawn tutorial

--- Ryddelmyst/Source/Ryddelmyst/Private/MollyPawn.cpp
+++ Ryddelmyst/Source/Ryddelmyst/Private/MollyPawn.cpp
@@ -2,14 +2,26 @@
 
 
 #include "MollyPawn.h"
-
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 AMollyPawn::AMollyPawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	// Set this pawn to be controlled by the lowest-numbered player
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+	// Dummy root Component we can attach things to
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	// Create camera and visible object to represent our Molly
+	UCameraComponent* MollyCam = CreateDefaultSubobject<UCameraComponent>(TEXT("MollyCam"));
+	VisibleMollyComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TheVisibleMolly"));
+	// Attach camera to our root component, and then offset and rotate the camera
+	MollyCam->SetupAttachment(RootComponent);
+	MollyCam->SetRelativeLocation(FVector(-250.0f, 0.0f, 250.0f));
+	MollyCam->SetRelativeRotation(FRotator(-45.0f, 0.0f, 0.0f));
+	// Attach visible object to our root component
+	VisibleMollyComponent->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
