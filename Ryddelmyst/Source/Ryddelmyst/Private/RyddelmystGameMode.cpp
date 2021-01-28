f0ae7176d38d341dffb7b7073b1d045a56f1fb83
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2021-01-27 21:00:19 -0500

    updating gitignore and removing .vscode junk

--- Ryddelmyst/Source/Ryddelmyst/Private/RyddelmystGameMode.cpp
+++ Ryddelmyst/Source/Ryddelmyst/Private/RyddelmystGameMode.cpp
@@ -9,7 +9,7 @@ ARyddelmystGameMode::ARyddelmystGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/RyddelMyst_Assets/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Ryddelmyst_Assets/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
