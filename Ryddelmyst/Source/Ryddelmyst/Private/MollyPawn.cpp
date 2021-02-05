22bdca46427d65f56e46698fc20e08ad6985baff
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2021-02-04 21:35:59 -0500

    working on comp and collision tut, and I def broke AMollyPawn::tick

--- Ryddelmyst/Source/Ryddelmyst/Private/MollyPawn.cpp
+++ Ryddelmyst/Source/Ryddelmyst/Private/MollyPawn.cpp
@@ -3,6 +3,11 @@
 
 #include "MollyPawn.h"
 #include "Camera/CameraComponent.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Components/SphereComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
 AMollyPawn::AMollyPawn() :
@@ -13,17 +18,45 @@ fMoveTime(0.0f)
 	PrimaryActorTick.bCanEverTick = true;
 	// Set this pawn to be controlled by the lowest-numbered player
 	AutoPossessPlayer = EAutoReceiveInput::Player0;
-	// Dummy root Component we can attach things to
-	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
-	// Create camera and visible object to represent our Molly
-	UCameraComponent* MollyCam = CreateDefaultSubobject<UCameraComponent>(TEXT("MollyCam"));
-	VisibleMollyComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TheVisibleMolly"));
-	// Attach camera to our root component, and then offset and rotate the camera
-	MollyCam->SetupAttachment(RootComponent);
-	MollyCam->SetRelativeLocation(FVector(-200.0f, 0.0f, 200.0f));
-	MollyCam->SetRelativeRotation(FRotator(-20.0f, 0.0f, 0.0f));
-	// Attach visible object to our root component
-	VisibleMollyComponent->SetupAttachment(RootComponent);
+	// Sphere shape will serve as our root component
+	USphereComponent* SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("RootSphereComponent"));
+	RootComponent = SphereComponent;
+	SphereComponent->InitSphereRadius(40.0f);
+    SphereComponent->SetCollisionProfileName(TEXT("MollyPawnPresence"));
+	// Create and position a mesh component so we can see where our spherical Molly is
+    UStaticMeshComponent* SphereVisual = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TheVisibleMolly"));
+    SphereVisual->SetupAttachment(RootComponent);
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
+    if (SphereVisualAsset.Succeeded())
+    {
+        SphereVisual->SetStaticMesh(SphereVisualAsset.Object);
+		// TODO: why is it necessary to move the sphere mesh 40 units down on Z to line up with the center of the sphere component?
+        SphereVisual->SetRelativeLocation(FVector(0.0f, 0.0f, -40.0f));
+		// Our sphere component has a radius of 40 units and the startercontent sphere mesh is 50, so scale it down by 20%
+        SphereVisual->SetWorldScale3D(FVector(0.8f));
+    }
+	// Create a particle system that we can activate or deactivate
+    MollyParticles = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("MollyMovementParticles"));
+    MollyParticles->SetupAttachment(SphereVisual);
+    MollyParticles->bAutoActivate = false;
+	// Offset slighlty from bottom-center of mesh to improve visibility
+	// TODO: how come we're moving up from our initial Z pos here and down in the adjustment of the mesh itself above?  What is considered origin in UE4?
+    MollyParticles->SetRelativeLocation(FVector(-20.0f, 0.0f, 20.0f));
+    static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticleAsset(TEXT("/Game/StarterContent/Particles/P_Fire.P_Fire"));
+    if (ParticleAsset.Succeeded())
+    {
+        MollyParticles->SetTemplate(ParticleAsset.Object);
+    }
+	 // Use a spring arm to give the camera smooth, natural-feeling motion.
+    USpringArmComponent* SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraAttachmentArm"));
+    SpringArm->SetupAttachment(RootComponent);
+    SpringArm->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
+    SpringArm->TargetArmLength = 400.0f;
+    SpringArm->bEnableCameraLag = true;
+    SpringArm->CameraLagSpeed = 3.0f;
+	// Create a camera and attach to our spring arm
+    UCameraComponent* Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("MollyCam"));
+    Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
 }
 
 // Called when the game starts or when spawned
@@ -39,7 +72,7 @@ void AMollyPawn::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	{
-		float CurrentScale = VisibleMollyComponent->GetComponentScale().X;
+		float CurrentScale = RootComponent->GetComponentScale().X;
 		if (bGrowing)
 		{
 			if(fMoveTime > 0 && fMoveTime <= kSPECIAL_MOVE_THRESHOLD)
@@ -60,7 +93,7 @@ void AMollyPawn::Tick(float DeltaTime)
 		}
 		// Make sure we never drop below our starting size, or increase past double size.
 		CurrentScale = FMath::Clamp(CurrentScale, 1.0f, 2.0f);
-		VisibleMollyComponent->SetWorldScale3D(FVector(CurrentScale));
+		RootComponent->SetWorldScale3D(FVector(CurrentScale));
 	}
 
 	// Handle movement based on our "MoveX" and "MoveY" axes
