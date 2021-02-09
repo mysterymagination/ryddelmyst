ddbf3ecb096be48d6303a789a80111ca3ba5d987
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2021-02-08 22:05:48 -0500

    almost finished with collision and movement tut

--- Ryddelmyst/Source/Ryddelmyst/Public/MollyPawn.h
+++ Ryddelmyst/Source/Ryddelmyst/Public/MollyPawn.h
@@ -15,18 +15,22 @@ class RYDDELMYST_API AMollyPawn : public APawn
 public:
 	// Sets default values for this pawn's properties
 	AMollyPawn();
-
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	/**
+	 * Returns our special MollyPawnMovementComponent
+	 */ 
+	virtual UPawnMovementComponent* GetMovementComponent() const override;
 
 	//Input functions
-	void Move_XAxis(float AxisValue);
-	void Move_YAxis(float AxisValue);
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+	void Turn(float AxisValue);
 	void StartGrowing();
 	void StopGrowing();
+	void ParticleToggle();
 
 protected:
 	// Called when the game starts or when spawned
@@ -49,5 +53,5 @@ public:
 	/**
 	 * Amount of time in seconds that player is allowed in which to trigger the special grow action after starting a move axis input
 	 */ 
-	constexpr static const float kSPECIAL_MOVE_THRESHOLD = 0.3f;
+	constexpr static const float kSPECIAL_MOVE_THRESHOLD = 0.2f;
 };
