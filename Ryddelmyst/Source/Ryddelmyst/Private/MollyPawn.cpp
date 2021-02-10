ae90bac223acbe0a62a30e75a7ec74371bebf410
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2021-02-09 22:26:53 -0500

    adapted accel code to movement comp

--- Ryddelmyst/Source/Ryddelmyst/Private/MollyPawn.cpp
+++ Ryddelmyst/Source/Ryddelmyst/Private/MollyPawn.cpp
@@ -11,8 +11,7 @@
 
 // Sets default values
 AMollyPawn::AMollyPawn() :
-bGrowing(false),
-fMoveTime(0.0f)
+bGrowing(false)
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -22,41 +21,41 @@ fMoveTime(0.0f)
 	USphereComponent* SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("RootSphereComponent"));
 	RootComponent = SphereComponent;
 	SphereComponent->InitSphereRadius(40.0f);
-    SphereComponent->SetCollisionProfileName(TEXT("MollyPawnPresence"));
+	SphereComponent->SetCollisionProfileName(TEXT("MollyPawnPresence"));
 	// Create and position a mesh component so we can see where our spherical Molly is
-    UStaticMeshComponent* SphereVisual = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TheVisibleMolly"));
-    SphereVisual->SetupAttachment(RootComponent);
-    static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
-    if (SphereVisualAsset.Succeeded())
-    {
-        SphereVisual->SetStaticMesh(SphereVisualAsset.Object);
+	UStaticMeshComponent* SphereVisual = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TheVisibleMolly"));
+	SphereVisual->SetupAttachment(RootComponent);
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
+	if (SphereVisualAsset.Succeeded())
+	{
+		SphereVisual->SetStaticMesh(SphereVisualAsset.Object);
 		// TODO: why is it necessary to move the sphere mesh 40 units down on Z to line up with the center of the sphere component?
-        SphereVisual->SetRelativeLocation(FVector(0.0f, 0.0f, -40.0f));
+		SphereVisual->SetRelativeLocation(FVector(0.0f, 0.0f, -40.0f));
 		// Our sphere component has a radius of 40 units and the startercontent sphere mesh is 50, so scale it down by 20%
-        SphereVisual->SetWorldScale3D(FVector(0.8f));
-    }
+		SphereVisual->SetWorldScale3D(FVector(0.8f));
+	}
 	// Create a particle system that we can activate or deactivate
-    MollyParticles = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("MollyMovementParticles"));
-    MollyParticles->SetupAttachment(SphereVisual);
-    MollyParticles->bAutoActivate = false;
+	MollyParticles = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("MollyMovementParticles"));
+	MollyParticles->SetupAttachment(SphereVisual);
+	MollyParticles->bAutoActivate = false;
 	// Offset slighlty from bottom-center of mesh to improve visibility
 	// TODO: how come we're moving up from our initial Z pos here and down in the adjustment of the mesh itself above?  What is considered origin in UE4?
-    MollyParticles->SetRelativeLocation(FVector(-20.0f, 0.0f, 20.0f));
-    static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticleAsset(TEXT("/Game/StarterContent/Particles/P_Fire.P_Fire"));
-    if (ParticleAsset.Succeeded())
-    {
-        MollyParticles->SetTemplate(ParticleAsset.Object);
-    }
+	MollyParticles->SetRelativeLocation(FVector(-20.0f, 0.0f, 20.0f));
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticleAsset(TEXT("/Game/StarterContent/Particles/P_Fire.P_Fire"));
+	if (ParticleAsset.Succeeded())
+	{
+		MollyParticles->SetTemplate(ParticleAsset.Object);
+	}
 	 // Use a spring arm to give the camera smooth, natural-feeling motion.
-    USpringArmComponent* SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraAttachmentArm"));
-    SpringArm->SetupAttachment(RootComponent);
-    SpringArm->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
-    SpringArm->TargetArmLength = 400.0f;
-    SpringArm->bEnableCameraLag = true;
-    SpringArm->CameraLagSpeed = 3.0f;
+	USpringArmComponent* SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraAttachmentArm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
+	SpringArm->TargetArmLength = 400.0f;
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->CameraLagSpeed = 3.0f;
 	// Create a camera and attach to our spring arm
-    UCameraComponent* Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("MollyCam"));
-    Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	UCameraComponent* Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("MollyCam"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
 	// Create an instance of our movement component, and tell it to update the root.
 	MollyMovementComponent = CreateDefaultSubobject<UMollyPawnMovementComponent>(TEXT("MollyMovementComponent"));
 	MollyMovementComponent->UpdatedComponent = RootComponent;
@@ -78,7 +77,9 @@ void AMollyPawn::Tick(float DeltaTime)
 		float CurrentScale = RootComponent->GetComponentScale().X;
 		if (bGrowing)
 		{
-			if(fMoveTime > 0 && fMoveTime <= kSPECIAL_MOVE_THRESHOLD)
+			// todo: migrate fMoveTime to MollyPawnMovementComponent and provide a getter to call here
+			float moveTime = MollyMovementComponent->getMoveTime();
+			if(moveTime > 0 && moveTime <= kSPECIAL_MOVE_THRESHOLD)
 			{
 				// Special growth action to 2x immediately
 				CurrentScale = 2.0f;
@@ -98,28 +99,11 @@ void AMollyPawn::Tick(float DeltaTime)
 		CurrentScale = FMath::Clamp(CurrentScale, 1.0f, 2.0f);
 		RootComponent->SetWorldScale3D(FVector(CurrentScale));
 	}
-
-	// Handle movement based on our "MoveX" and "MoveY" axes
-	{
-		if (!CurrentVelocity.IsZero())
-		{
-			UE_LOG(LogTemp, Warning, TEXT("Tick; deltaT says %f and movetime says %f"), DeltaTime, fMoveTime);
-			fMoveTime += DeltaTime;
-			fMoveTime = FMath::Clamp(fMoveTime, 0.0f, 0.3f);
-			FVector NewLocation = GetActorLocation() + (CurrentVelocity * fMoveTime);
-			SetActorLocation(NewLocation);
-		}
-		else 
-		{
-			fMoveTime = 0.0f;
-		}
-	}
-
 }
 
 UPawnMovementComponent* AMollyPawn::GetMovementComponent() const
 {
-    return MollyMovementComponent;
+	return MollyMovementComponent;
 }
 
 // Called to bind functionality to input
@@ -130,7 +114,7 @@ void AMollyPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 	// Respond when our "Grow" key is pressed or released.
 	InputComponent->BindAction("Grow", IE_Pressed, this, &AMollyPawn::StartGrowing);
 	InputComponent->BindAction("Grow", IE_Released, this, &AMollyPawn::StopGrowing);
-	InputComponent->BindAction("ParticleToggle", IE_Pressed, this, &AMollyPawn::ParticleToggle);
+	InputComponent->BindAction("Sparkle", IE_Pressed, this, &AMollyPawn::ParticleToggle);
 
 	// Respond every frame to the values of our two movement axes, "MoveX" and "MoveY".
 	InputComponent->BindAxis("MoveForward", this, &AMollyPawn::MoveForward);
@@ -142,6 +126,7 @@ void AMollyPawn::MoveForward(float AxisValue)
 {
 	if (MollyMovementComponent && (MollyMovementComponent->UpdatedComponent == RootComponent))
 	{
+		UE_LOG(LogTemp, Display, TEXT("MollyPawn::MoveForward; forward vector is %s and axisvalue is %f"), *GetActorForwardVector().ToString(), AxisValue);
 		MollyMovementComponent->AddInputVector(GetActorForwardVector() * AxisValue);
 	}
 }
@@ -150,6 +135,7 @@ void AMollyPawn::MoveRight(float AxisValue)
 {
 	if (MollyMovementComponent && (MollyMovementComponent->UpdatedComponent == RootComponent))
 	{
+		UE_LOG(LogTemp, Display, TEXT("MollyPawn::MoveRight; right vector is %s and axisvalue is %f"), *GetActorRightVector().ToString(), AxisValue);
 		MollyMovementComponent->AddInputVector(GetActorRightVector() * AxisValue);
 	}
 }
--- Ryddelmyst/Source/Ryddelmyst/Public/MollyPawnMovementComponent.h
+++ Ryddelmyst/Source/Ryddelmyst/Public/MollyPawnMovementComponent.h
@@ -15,4 +15,10 @@ class RYDDELMYST_API UMollyPawnMovementComponent : public UPawnMovementComponent
 	GENERATED_BODY()
 public:
 	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+	float getMoveTime();
+private:
+	/**
+	 * Aggregates deltatime from incoming frames wherein we've continuously moved
+	 */  
+	float fMoveTime = 0.0f;
 };
