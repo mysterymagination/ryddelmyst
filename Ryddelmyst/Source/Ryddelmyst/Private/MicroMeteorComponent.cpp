c0265a9ca279cef61ba2057394effb81526c1c9a
mysterymagination <creswel2@gmail.com>
2021-04-05 00:37:18 -0400

    meteoric mystery!

--- Ryddelmyst/Source/Ryddelmyst/Private/MicroMeteorComponent.cpp
+++ Ryddelmyst/Source/Ryddelmyst/Private/MicroMeteorComponent.cpp
@@ -57,7 +57,7 @@ void UMicroMeteorComponent::BeginPlay()
 	Super::BeginPlay();
 
 	// adjust timing with a little instance-unique rando action
-	fMaxLifeTime += FMath::RandRange(-5, 5);
+	fMaxLifeTime += FMath::RandRange(-2, 5);
 	fMaxLaunchedLifeTime += FMath::RandRange(-2, 2);
 
 	// Sphere shape will serve as our root component
@@ -104,19 +104,18 @@ void UMicroMeteorComponent::TickComponent(float DeltaTime, ELevelTick TickType, 
 			else
 			{
 				UE_LOG(LogTemp, Warning, TEXT("%s MicroMeteor::TickComponent; launching inversely"), *FDateTime::UtcNow().ToString());
-				// take off straight on orthogonal, moving linearly at 100 units/second at our current angle
-				
 				// first translate out on our existing launch vector's XY
-				LaunchedOffset += FVector(20 * DeltaTime * LaunchedOffset.X, 20 * DeltaTime * LaunchedOffset.Y, 0.0f);
+				LaunchedOffset += FVector(DeltaTime * LaunchedOffset.X, DeltaTime * LaunchedOffset.Y, 0.0f);
 				// now perform rotation to undo the continuing orbit rotation of the torch our meteor was orbitting such that we can use SetRelativeLocation and maintain a consistent unrotated launch vector.
 				// accumulate based on torch yaw this frame
-				OrbitAccumulator.Yaw += pOrbitted->getOrbitYaw();
+				///OrbitAccumulator.Yaw += pOrbitted->getOrbitYaw();
 				// invert orbit accumulator rotator
 				FRotator InverseTorchOrbit = OrbitAccumulator.GetInverse();
 				UE_LOG(LogTemp, Warning, TEXT("MicroMeteor::TickComponent; our acc yaw is %f and we'll be rotating our launch vector by %s"), OrbitAccumulator.Yaw, *InverseTorchOrbit.ToString());
 				// rotate the LaunchOffset vector by the inverted orbit accumulator rotator; the orbit accumulator needs to live here and we just publish the Yaw mod we make per frame up in the torch so that we know the rotation that has occurred (which is what we seek to remove) per meteor instance.  
 				LaunchedOffset = InverseTorchOrbit.RotateVector(LaunchedOffset);
 				// now that we've translated progression along the launch vector and got a rotation back to our launch point relative to the orbitted body baked into the transform, call SetRelativeLocation()
+				// todo: I think the spiral-y problem I'm seeing with meteor launch now is down to the fact that the yaw we acc represents the torch's rotation around molly, but we're applying the inverse of that rotation to the meteor's launch vector relative to the torch -- effectively rotating around the torch (eventually?).  Perhaps if we attached the meteor to molly and then did this same rotation dance we might be able to use SetRelativeLocation() here?
 				SetRelativeLocation(LaunchedOffset);
 
 				// todo: alternatively, take a snapshot of the torch and meteor world space locations when launch occurs and call the delta between those two points your launch vector.  Progress that vector fractionally on XY and boom done.
@@ -165,6 +164,11 @@ void UMicroMeteorComponent::MeteoricLaunch()
 {
 	// todo: fire up with fire particle fx
 	// acquire launch init relative pos and flip launched trigger
+
+	// todo: huh?  this actually appears to work!  But why... Maybe it's another one of those tricky keepWorldPos flag things where, since the parent transforms are updating independently and we're setting our location relative to them we only need to specify our initial offset and apply it per frame?  But I thought the whole reason to acc yaw was to combat the fact that the torch continues it's rotation around molly with respect to the pos it was in when the meteor launched?
+	// EDIT: aha!  It doesn't actually work, the spiral issue was just much more subtle and with the camera pos and 20 units/sec launch speed I couldn't see the spiral begin.  At 1 unit/sec launch I can see it clearly.
+	OrbitAccumulator.Yaw = static_cast<UIounTorchComponent*>(this->GetAttachParent())->getOrbitYaw();
+	
 	LaunchedOffset = GetRelativeLocation();
 	bLaunched = true;
 }
