9da7af51feb2242a0530ca387467c33dd85f138e
mysterymagination <creswel2@gmail.com>
2021-04-19 22:31:14 -0400

    cleanup and bringin back three meteors

--- Ryddelmyst/Source/Ryddelmyst/Private/MicroMeteorComponent.cpp
+++ Ryddelmyst/Source/Ryddelmyst/Private/MicroMeteorComponent.cpp
@@ -69,21 +69,12 @@ void UMicroMeteorComponent::TickComponent(float DeltaTime, ELevelTick TickType, 
 		}
 		else
 		{
-			// todo: pretty sure my meteor is getting teleported someplace out of sight since it seems to disappear as soon as launch begins and then reports being destroyed sometime much later
-			// first translate out on our existing launch vector's XY
-			// at 100 units per second
-			//LaunchedOffset += FVector(DeltaTime * 100.0f, DeltaTime * 100.0f, 0.0f); // so the issue here was literally acceleration -- we'd apply a set slope mod with the LaunchedOffset + current loc below (velocity) but then here we'd be increasing the velocity resulting in acceleration and a curve instead of the desired line of motion.
-
-			UE_LOG(LogTemp, Warning, TEXT("TickComponent; progressive launch offset says %s"), *LaunchVector.ToString());
 			SetWorldLocation(GetComponentLocation() + LaunchVector);
-			UE_LOG(LogTemp, Warning, TEXT("TickComponent; after applying launch offset of %s, our meteor coords are %s"), *LaunchVector.ToString(), *GetComponentLocation().ToString());
 
 			// update launched time tracker
 			fLaunchedLifeTimer += DeltaTime;
-			UE_LOG(LogTemp, Warning, TEXT("MicroMeteor::TickComponent; we've been launched for %f"), fLaunchedLifeTimer);
 			if (fLaunchedLifeTimer >= fMaxLaunchedLifeTime)
 			{
-				UE_LOG(LogTemp, Warning, TEXT("MicroMeteor::TickComponent; we've been launched for %f, which is quite long enough (gte max launch time of %f) -- self-destruct!"), fLaunchedLifeTimer, fMaxLaunchedLifeTime);
 				// todo: explosion particle fx before destroy?
 				TArray<USceneComponent*> childrenArray;
 				GetChildrenComponents(true, childrenArray);
@@ -97,16 +88,13 @@ void UMicroMeteorComponent::TickComponent(float DeltaTime, ELevelTick TickType, 
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("TickComponent; meteor still orbitting because fLifeTimer is only %f while allowed max is %f"), fLifeTimer, fMaxLifeTime);
 		// orbit motion @ 60 degrees/second
 		float OrbitRotation = DeltaTime * 160.0f;
 		FRotator OffsetVecRot(0.0f, 0.0f, 0.0f);
 		OffsetVecRot.Yaw = OrbitRotation;
-		UE_LOG(LogTemp, Warning, TEXT("MicroMeteor::TickComponent; OffsetVecRot says %s"), *OffsetVecRot.ToString());
 		OrbitOffset = OffsetVecRot.RotateVector(OrbitOffset);
 
 		// progress orbit path
-		UE_LOG(LogTemp, Warning, TEXT("MicroMeteor::TickComponent; still orbitting... OrbitOffset says %s"), *OrbitOffset.ToString());
 		SetRelativeLocation(OrbitOffset);
 	}		
 }
@@ -118,6 +106,7 @@ void UMicroMeteorComponent::MeteoricLaunch()
 	DetachFromComponent(FDetachmentTransformRules::KeepWorldTransform);
 	// acquire launch vector via init launch meteor world pos minus launch orbitted body pos and flip launched trigger
 	UE_LOG(LogTemp, Warning, TEXT("TickComponent; init launch world coords of meteor are %s and launch world coords of orbitted body are %s so our delta vec to be used for init launch offset vec is %s"), *GetComponentLocation().ToString(), *pOrbitted->GetComponentLocation().ToString(), *(GetComponentLocation() - pOrbitted->GetComponentLocation()).ToString());
+	// todo: currently this gives us a meteoric speed equal to the maggie of this diff vector, which is not very convenient -- I'd like to be able to tweak it in the editor to go @ N map units per second and then only the direction will be given by this vector.
 	LaunchVector = GetComponentLocation() - pOrbitted->GetComponentLocation();
 	// we don't want to progress on Z, so leave launch vector Z at 0
 	// assuming we're going to be adding the launch vector to the current
