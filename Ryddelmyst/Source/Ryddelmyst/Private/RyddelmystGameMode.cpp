f36a2699f28509902d0115a470b0890337ed0804
mysterymagination <creswel2@gmail.com>
2021-06-15 23:35:16 -0400

    added debug message to gamemode, learned what gamemode is!

--- Ryddelmyst/Source/Ryddelmyst/Private/RyddelmystGameMode.cpp
+++ Ryddelmyst/Source/Ryddelmyst/Private/RyddelmystGameMode.cpp
@@ -15,3 +15,14 @@ ARyddelmystGameMode::ARyddelmystGameMode()
 	// use our custom HUD class
 	HUDClass = ARyddelmystHUD::StaticClass();
 }
+
+void ARyddelmystGameMode::StartPlay()
+{
+	Super::StartPlay();
+
+	check(GEngine != nullptr);
+
+	// Display a debug message for five seconds. 
+	// The -1 "Key" value argument prevents the message from being updated or refreshed.
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello World, this is RyddelmystGameMode!"));
+}
