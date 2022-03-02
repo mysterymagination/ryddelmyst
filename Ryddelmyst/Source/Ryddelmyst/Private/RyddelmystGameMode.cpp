5a59fe4f998ac392a893d0d0ee631f677085ebd7
mysterymagination <creswel2@gmail.com>
2022-03-01 21:51:24 -0500

    moved the egg and fire bead into more interesting/hidden locations and added game state check in gamemode

--- Ryddelmyst/Source/Ryddelmyst/Private/RyddelmystGameMode.cpp
+++ Ryddelmyst/Source/Ryddelmyst/Private/RyddelmystGameMode.cpp
@@ -3,13 +3,15 @@
 #include "RyddelmystGameMode.h"
 #include "RyddelmystHUD.h"
 #include "RyddelmystCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
 #include "UObject/ConstructorHelpers.h"
 
 ARyddelmystGameMode::ARyddelmystGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Ryddelmyst_Assets/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Ryddelmyst_Assets/Blueprints/BP_Maya"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
@@ -25,4 +27,58 @@ void ARyddelmystGameMode::StartPlay()
 	// Display a debug message for five seconds. 
 	// The -1 "Key" value argument prevents the message from being updated or refreshed.
 	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello World, this is RyddelmystGameMode!"));
+}
+
+void ARyddelmystGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	SetCurrentState(EGamePlayState::EPlaying);
+	PlayerCharacter = Cast<ARyddelmystCharacter>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
+
+void ARyddelmystGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (PlayerCharacter)
+	{
+		if (FMath::IsNearlyZero(PlayerCharacter->GetHealth(), 0.001f))
+		{
+			SetCurrentState(EGamePlayState::EGameOver);
+			HandleNewState(EGamePlayState::EGameOver);
+		}
+	}
+}
+
+EGamePlayState ARyddelmystGameMode::GetCurrentState() const
+{
+	return CurrentState;
+}
+
+void ARyddelmystGameMode::SetCurrentState(EGamePlayState NewState)
+{
+	CurrentState = NewState;
+}
+
+void ARyddelmystGameMode::HandleNewState(EGamePlayState NewState)
+{
+	switch (NewState)
+	{
+		case EGamePlayState::EPlaying:
+		{
+			// do nothing
+		}
+		break;
+		case EGamePlayState::EGameOver:
+		{
+			// todo: what does this do in PIE mode?  Anyway, need an actual UI saying the game is over and a restart button
+			FGenericPlatformMisc::RequestExit(false);
+		}
+		break;
+		default:
+		case EGamePlayState::EUnknown:
+		{
+			// do nothing
+		}
+		break;
+	}
 }
--- Ryddelmyst/Source/Ryddelmyst/Public/RyddelmystGameMode.h
+++ Ryddelmyst/Source/Ryddelmyst/Public/RyddelmystGameMode.h
@@ -4,7 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "RyddelmystCharacter.h"
 #include "RyddelmystGameMode.generated.h"
+
+//enum to store the current state of gameplay
+UENUM()
+enum class EGamePlayState : uint8
+{
+	EPlaying,
+	EGameOver,
+	EUnknown
+};
 
 UCLASS(minimalapi)
 class ARyddelmystGameMode : public AGameModeBase
@@ -14,6 +24,21 @@ class ARyddelmystGameMode : public AGameModeBase
 public:
 	ARyddelmystGameMode();
 	virtual void StartPlay() override;
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	ARyddelmystCharacter* PlayerCharacter;
+	/** Returns the current playing state */
+	UFUNCTION(BlueprintPure, Category = "Health")
+	EGamePlayState GetCurrentState() const;
+	/** Sets a new playing state */
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	void SetCurrentState(EGamePlayState NewState);
+
+private:
+	/**Keeps track of the current playing state */
+	EGamePlayState CurrentState;
+	/**Handle any function calls that rely upon changing our gameplay state */
+	void HandleNewState(EGamePlayState NewState);
 };
 
 
