107e185c792cbac1dc90a3eab70d683dcc259e99
mysterymagination <creswel2@gmail.com>
2022-03-16 21:56:48 -0400

    fleshing out Item interface

--- Ryddelmyst/Source/Ryddelmyst/Public/Item.h
+++ Ryddelmyst/Source/Ryddelmyst/Public/Item.h
@@ -14,12 +14,34 @@ class UItem : public UInterface
 };
 
 /**
- * 
+ * Interface for Actors that can be added to the player's inventory and used from there by the player
  */
 class RYDDELMYST_API IItem
 {
 	GENERATED_BODY()
 
-	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
+	/**
+	 * Function run when the player first picks up the item from the stage and places it in their inventory
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Item")
+	void OnPickup();
+
+	/**
+	 * Function run when the player drops the item from inventory back into the stage
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Item")
+	void OnDrop();
+
+	/**
+	 * Function run when the player uses the item from their inventory
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Item")
+	void OnUse();
+
+	/**
+	 * Returns the icon that should represent this item in the player's inventory UI
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Item")
+	UTexture2D* GetDisplayIcon();
 };
