a514967581fd6f7d95a9a20d321278f64a334112
mysterymagination <creswel2@gmail.com>
2022-03-19 23:28:07 -0400

    working on inventory selection, use, and data model

---
+++ Ryddelmyst/Source/Ryddelmyst/Public/ItemActor.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.h"
+#include "ItemActor.generated.h"
+
+UCLASS()
+class RYDDELMYST_API AItemActor : public AActor
+{
+	GENERATED_BODY()
+	
+private:
+	UItem* Item;
+
+public:	
+	// Sets default values for this actor's properties
+	AItemActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	UItem* GetItem();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
+	TSubclassOf<UItem> ItemType;
+};
