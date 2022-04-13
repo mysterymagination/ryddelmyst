7886dd25fa715046c8819ac1d24b60851f2ff4a4
mysterymagination <creswel2@gmail.com>
2022-04-13 17:56:11 -0400

    adding data driven treasure

---
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Private/TreasureChest.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TreasureChest.h"
+
+// Sets default values
+ATreasureChest::ATreasureChest()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATreasureChest::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATreasureChest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
