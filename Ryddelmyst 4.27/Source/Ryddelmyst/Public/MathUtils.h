1f83b655e4d24412be981816a12f0bf6378c0710
mysterymagination <creswel2@gmail.com>
2022-04-14 17:13:36 -0400

    mess of an init draft of magic damage and magical modding

---
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Public/MathUtils.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * Math utility functions
+ */
+class RYDDELMYST_API MathUtils
+{
+public:
+	static float RollNdM()
+	{
+		// random number generation in range of N to NM
+	}
+};
