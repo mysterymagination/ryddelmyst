1518718083a293dcbd52ddbb3130ab189f871f6a
mysterymagination <creswel2@gmail.com>
2022-04-13 21:18:44 -0400

    setting up metamagic

---
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Private/DiademHellfireMightItem.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DiademHellfireMightItem.h"
+
+void UDiademHellfireMightItem::OnEquip(AActor* EquippedActor)
+{
+	// todo: toss a lambda (std::function or maybe TFunctionRef) over to the EquippedActor cast to RyddelmystCharacter which she'll then apply to fireballs she generates; she'll store this in her MetamagicFire field.
+}
+
