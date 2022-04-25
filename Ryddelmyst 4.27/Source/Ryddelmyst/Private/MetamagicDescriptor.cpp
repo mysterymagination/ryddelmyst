2825baadce8c4a87e932a0e2d179a74a596c9706
mysterymagination <creswel2@gmail.com>
2022-04-25 19:57:09 -0400

    metametametamagic

---
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Private/MetamagicDescriptor.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MetamagicDescriptor.h"
+
+MetamagicDescriptor::MetamagicDescriptor()
+{
+}
+
+MetamagicDescriptor::~MetamagicDescriptor()
+{
+}
