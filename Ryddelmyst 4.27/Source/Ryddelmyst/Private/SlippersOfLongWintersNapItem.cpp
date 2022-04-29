b828fc72f2655119e6f737a4d83d56e4e42597a8
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-04-29 14:23:59 -0400

    starting in on actually using this metamagical beast I hath wrought for some reason

--- Ryddelmyst 4.27/Source/Ryddelmyst/Private/SlippersOfLongWintersNapItem.cpp
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Private/SlippersOfLongWintersNapItem.cpp
@@ -12,7 +12,7 @@ void USlippersOfLongWintersNapItem::OnEquip_Implementation(AActor* EquippedActor
 	ARyddelmystCharacter* Character = Cast<ARyddelmystCharacter>(EquippedActor);
 	if (Character)
 	{
-		Character->GetMetamagicMap()[ARyddelmystCharacter::ID_SPELL_SNOWBALL][USlippersOfLongWintersNapItem::ID][ARyddelmystCharacter::ID_SPELL_PHASE_EVOCATION][ARyddelmystCharacter::ID_METAMAGIC_CATEGORY_ATTR] = [](ASnowball* Snowball)
+		Character->GetMetamagicMap()[std::string(TCHAR_TO_UTF8(*ASnowball::StaticClass()->GetName()))][std::string(TCHAR_TO_UTF8(*USlippersOfLongWintersNapItem::StaticClass()->GetName()))][ARyddelmystCharacter::ID_SPELL_PHASE_EVOCATION][ARyddelmystCharacter::ID_METAMAGIC_CATEGORY_ATTR] = [](ASnowball* Snowball)
 		{
 			UE_LOG(LogTemp, Warning, TEXT("Metamagic Ice lambda; attr modifier effect"));
 			Snowball->SetFreezeDuration(2.f);
