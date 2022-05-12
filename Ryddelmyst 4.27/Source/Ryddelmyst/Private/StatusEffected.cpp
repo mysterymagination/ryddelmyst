ff9af8c1d2062b3a653227d30b8f03132479dde3
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-05-11 20:31:54 -0400

    stubs for status effects and interface for Actors that can be effected by them

---
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Private/StatusEffected.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StatusEffected.h"
+
+
+// Add default functionality here for any IStatusEffected functions that are not pure virtual.
