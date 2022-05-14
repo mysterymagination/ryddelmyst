cba87e0b2e5b873f67eec100dd466e74eea4900c
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-05-13 20:51:43 -0400

    more status effect fleshing out

--- Ryddelmyst 4.27/Source/Ryddelmyst/Public/StatusEffect.h
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Public/StatusEffect.h
@@ -3,13 +3,26 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "StatusEffect.generated.h"
 
 /**
  * Parent class for all status effects e.g. frozen, burned etc.
  */
-class RYDDELMYST_API StatusEffect
+UCLASS(BlueprintType, Blueprintable)
+class RYDDELMYST_API UStatusEffect : public UObject
 {
+	GENERATED_BODY()
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Magic", meta = (AllowPrivateAccess = "true"))
+	FString Id;
+
 public:
-	StatusEffect();
-	~StatusEffect();
+	UFUNCTION(BlueprintCallable, Category = "Magic")
+	void OnEffect(AActor* EffectedActor){};
+
+	UFUNCTION(BlueprintCallable, Category = "Magic")
+	void SetId(const FString& Id){ this.Id = Id; };
+
+	UFUNCTION(BlueprintCallable, Category = "Magic")
+	const FString& GetId(){ return this.Id; };
 };
