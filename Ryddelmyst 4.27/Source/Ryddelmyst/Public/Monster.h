17960c07ed3735d58d6f7cb3aaec85329ae7dd82
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-05-13 20:51:43 -0400

    more status effect fleshing out

---
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Public/Monster.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/SkeletalMeshActor.h"
+#include "Monster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RYDDELMYST_API AMonster : public ASkeletalMeshActor
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
