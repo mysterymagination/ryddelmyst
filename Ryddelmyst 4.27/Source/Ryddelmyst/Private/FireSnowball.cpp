9884e56432b4ad5cd2ace71283ef1448c12fcda9
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-05-23 20:15:18 -0400

    refactoring snowballs such that elemental aspects are given entirely in subclasses, no more default cold snowball since we don't want default OnHit behavior to always install a frozen status effect

--- Ryddelmyst 4.27/Source/Ryddelmyst/Private/FireSnowball.cpp
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Private/FireSnowball.cpp
@@ -3,6 +3,8 @@
 
 #include "FireSnowball.h"
 #include "FireDamageType.h"
+#include "StatusEffected.h"
+#include "BurnedStatusEffect.h"
 
 AFireSnowball::AFireSnowball()
 {
@@ -13,16 +15,32 @@ AFireSnowball::AFireSnowball()
 		ProjectileMaterialInstance = UMaterialInstanceDynamic::Create(Material.Object, ProjectileMeshComponent);
 	}
 	ProjectileMeshComponent->SetMaterial(0, ProjectileMaterialInstance);
-	ProjectileMeshComponent->SetRelativeScale3D(FVector(0.09f, 0.09f, 0.09f));
-	ProjectileMeshComponent->SetupAttachment(RootComponent);
+	
 	// Use a fiery particle system
 	static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticleAsset(TEXT("/Game/Ryddelmyst_Assets/Particles/P_Fiery.P_Fiery"));
 	if (ParticleAsset.Succeeded())
 	{
 		SnowballParticles->SetTemplate(ParticleAsset.Object);
 	}
-
 	DamageType = UFireDamageType::StaticClass();
-	StatusEffect = CreateDefaultSubobject<UBurnedStatusEffect>(TEXT("FireSnowballBurnEffect"));
-	StatusEffect->SetId("BurnedStatusEffect");
+}
+
+void AFireSnowball::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	ASnowball::OnHit(HitComp, OtherActor, OtherComp, NormalImpulse, Hit);
+	// primary StatusEffect
+	if(OtherActor::StaticClass()->ImplementsInterface(UStatusEffected::StaticClass()))
+    {
+        // todo: does making OtherActor the outer/owner and then also adding the status effect to a uprop'd tarray inside OtherActor cause the StatusEffect to be doubly referenced by OtherActor?  I want the StatusEffect to be owned by the target TArray, which itself is owned by OtherActor; StatusEffects removed from that array should be garbage collected.
+        UBurnedStatusEffect* StatusEffect = NewObject<UBurnedStatusEffect>(OtherActor, UBurnedStatusEffect::StaticClass());
+        StatusEffect->SetId("BurnedStatusEffect");
+        StatusEffect->SetDuration(FreezeDuration);
+		StatusEffect->SetPeriod(0.5f);
+		// ongoing damage will be init calculated damage over 2
+		StatusEffect->SetDamage(Damage/2.f);
+        UE_LOG(LogTemp, Warning, TEXT("OnHit; applying primary StatusEffect with id %s"), *StatusEffect->GetId());
+        IStatusEffected::Execute_AddStatusEffect(OtherActor, StatusEffect);
+        // todo: should we apply effect directly here or lean on IStatusEffected::ProcessStatusEffects()?  It may need to depend on whether we're in real time or turn based mode at the moment, or maybe we should always run the apply code and the real time/turn based game mode switch will have to hook into StatusEffect objects somehow to turn off timers and replace with turn counts and convert any elapsed time to elapsed turns.
+        StatusEffect->OnEffectApplied(OtherActor);
+    }
 }
