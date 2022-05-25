8291d42b6910a156b78889083562ae764703dec5
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-05-24 20:54:39 -0400

    finishing up snowball refactor and starting in on ElectricSnowball's ShockedStatusEffect

--- Ryddelmyst 4.27/Source/Ryddelmyst/Private/ElectricSnowball.cpp
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Private/ElectricSnowball.cpp
@@ -3,6 +3,8 @@
 
 #include "ElectricSnowball.h"
 #include "LightningDamageType.h"
+#include "StatusEffected.h"
+#include "ShockedStatusEffect.h"
 
 // Sets default values
 AElectricSnowball::AElectricSnowball()
@@ -28,5 +30,24 @@ AElectricSnowball::AElectricSnowball()
 	MagicCost = 20.f;
 
 	DamageType = ULightningDamageType::StaticClass();
+}
+
+void AElectricSnowball::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	ASnowball::OnHit(HitComp, OtherActor, OtherComp, NormalImpulse, Hit);
+	// elemental StatusEffect
+	if(OtherActor::StaticClass()->ImplementsInterface(UStatusEffected::StaticClass()))
+    {
+        // todo: check that OtherActor implements the IStats interface and iff he does then pull down the stats for modification by ShockedStatusEffect (modifications which will be removed when the shocked effect ends)
+
+		// todo: does making OtherActor the outer/owner and then also adding the status effect to a uprop'd tarray inside OtherActor cause the StatusEffect to be doubly referenced by OtherActor?  I want the StatusEffect to be owned by the target TArray, which itself is owned by OtherActor; StatusEffects removed from that array should be garbage collected.
+        UShockedStatusEffect* StatusEffect = NewObject<UShockedStatusEffect>(OtherActor, UShockedStatusEffect::StaticClass());
+        StatusEffect->SetId("ShockedStatusEffect");
+        StatusEffect->SetDuration(ShockDuration);
+        UE_LOG(LogTemp, Warning, TEXT("OnHit; applying primary StatusEffect with id %s"), *StatusEffect->GetId());
+        IStatusEffected::Execute_AddStatusEffect(OtherActor, StatusEffect);
+        // todo: should we apply effect directly here or lean on IStatusEffected::ProcessStatusEffects()?  It may need to depend on whether we're in real time or turn based mode at the moment, or maybe we should always run the apply code and the real time/turn based game mode switch will have to hook into StatusEffect objects somehow to turn off timers and replace with turn counts and convert any elapsed time to elapsed turns.
+        StatusEffect->OnEffectApplied(OtherActor);
+    }
 }
 
