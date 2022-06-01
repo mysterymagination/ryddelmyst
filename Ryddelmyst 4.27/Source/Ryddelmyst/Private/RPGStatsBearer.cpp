7132969b8cfbf36910e48695d6c83fd8f32f3679
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-05-31 22:22:42 -0400

    cleaning up and refactoring my RPG stats stuff since BattleStats sounds like Battlecat and anything made He-Man related is fundamentally better than it was before

--- Ryddelmyst 4.27/Source/Ryddelmyst/Private/RPGStatsBearer.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "RPGStatsBearer.h"
-
-
-// Add default functionality here for any IRPGStats functions that are not pure virtual.
