2052ad400417cdb9ca325067092f359615256836
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-06-24 01:48:20 -0400

    adding stub for TrackPlayer in MonsterAI and set up timer to call it on editor defined period

--- Ryddelmyst 4.27/Source/Ryddelmyst/Public/MonsterAI.h
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Public/MonsterAI.h
@@ -13,8 +13,22 @@ UCLASS()
 class RYDDELMYST_API AMonsterAI : public AAIController
 {
 	GENERATED_BODY()
-	
-	
+	UPROPERTY()
+	FTimerHandle TrackingTimerHandle;
+
+protected:
+	/**
+	 * @brief The period at which we refresh our tracking behavior, in seconds.  If the AI finds the player and stops tracking, it will need to wait until this period is up to resume the chase.
+	 * 
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
+	float TrackingPeriod = 1.f;
+
+protected:
+	virtual void BeginPlay() override;
+
+public: 
+	void TrackPlayer();
 	
 	
 };
