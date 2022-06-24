b2ccbb3cb4b29b3d3e215f2c9f852aae9cd407e5
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-06-24 01:48:20 -0400

    adding stub for TrackPlayer in MonsterAI and set up timer to call it on editor defined period

--- Ryddelmyst 4.27/Source/Ryddelmyst/Private/MonsterAI.cpp
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Private/MonsterAI.cpp
@@ -4,5 +4,12 @@
 #include "MonsterAI.h"
 
 
+void AMonsterAI::BeginPlay()
+{
+    GetWorldTimerManager().SetTimer(TrackingTimerHandle, this, &AMonsterAI::TrackPlayer, TrackingPeriod, true);
+}
 
-
+void AMonsterAI::TrackPlayer()
+{
+    // todo: ape AI Move To node
+}
--- Ryddelmyst 4.27/Source/Ryddelmyst/Public/Monster.h
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Public/Monster.h
@@ -5,17 +5,47 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "MonsterAI.h"
+#include "BattleStats.h"
+#include "BattleStatsBearer.h"
+#include "StatusEffected.h"
 #include "Monster.generated.h"
 
 /**
  * A Pawn controlled by MonsterAI
  */
 UCLASS(BlueprintType, Blueprintable)
-class RYDDELMYST_API AMonster : public APawn
+class RYDDELMYST_API AMonster : public APawn, public IBattleStatsBearer, public IStatusEffected
 {
 	GENERATED_BODY()
-	
-	
+	UPROPERTY()
+	UBattleStats* MonsterStats;
+	UPROPERTY()
+	TArray<const UStatusEffect*> StatusEffects;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RPG")
+	TSubclassOf<UBattleStats> MonsterStatsType;
+
+public:
+	UBattleStats* GetStats_Implementation() { UE_LOG(LogTemp, Warning, TEXT("GetStats; getting monster stats")); return MonsterStats; }
+	void UpdateSpeed_Implementation() 
+	{ 
+		// todo: figure out speed for a generic MovementComponent and/or make a custom monster movementcomponent
+		UE_LOG(LogTemp, Warning, TEXT("UpdateSpeed; not yet implemented for monster")); 
+	}
+	void AddStatusEffect_Implementation(UStatusEffect* Effect) 
+	{ 
+		StatusEffects.Add(Effect); 
+	}
+	void RemoveStatusEffect_Implmenetation(const FString& EffectId) 
+	{ 
+		const UStatusEffect* StatusToRemove = *StatusEffects.FindByPredicate([&](const UStatusEffect* Effect)
+			{
+				return Effect->GetName().Equals(EffectId);
+			}
+		); 
+		StatusEffects.RemoveAt(StatusEffects.Find(StatusToRemove));
+	}	
 	
 	
 };
