59f3ff434e62eb592927cbe3553e919d4403296d
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-06-26 01:06:34 -0400

    made reaper treant a monster!

--- Ryddelmyst 4.27/Source/Ryddelmyst/Private/Monster.cpp
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Private/Monster.cpp
@@ -3,7 +3,7 @@
 
 #include "Monster.h"
 
-
-
-
-// todo: how does one modify the max speed of an AI Actor?  Only CharacterMovementComponent seems to expose speed setter functions... might need a custom subclass. EDIT: nope, CharacterMovementComponent is fine to use on AI Actors; the only distinction between a Pawn and a Character is that the latter has a Mesh, collision, and defined movement behavior.
+AMonster::AMonster()
+{
+    AIControllerClass = AMonsterAI::StaticClass();
+}
