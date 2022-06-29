f34a98db6cfd5eaed67d30bf664288fe695c45f9
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-06-28 21:52:02 -0400

    reaper treant chase AI is live!

--- Ryddelmyst 4.27/Source/Ryddelmyst/Private/Monster.cpp
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Private/Monster.cpp
@@ -5,5 +5,37 @@
 
 AMonster::AMonster()
 {
+    /* todo: overriding this in BP may not be working?  I'm not seeing BP_ReaperTreantAI doing anything
     AIControllerClass = AMonsterAI::StaticClass();
+    */
+   PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AMonster::BeginPlay()
+{
+	Super::BeginPlay();
+	if(MonsterStatsType)
+	{
+		MonsterStats = NewObject<UBattleStats>(this, MonsterStatsType);
+		UE_LOG(LogTemp, Warning, TEXT("BeginPlay; character stats set to %s"), *MonsterStats->ToString());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("BeginPlay; Monster stats undefined"));
+	}
+}
+
+// Called every frame
+void AMonster::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMonster::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
 }
