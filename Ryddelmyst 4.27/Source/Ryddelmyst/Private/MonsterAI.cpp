5e5df9225dc665c3a736a6ee095c3b4adaa37b69
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-06-29 17:44:47 -0400

    reaper treant basic behavior is looking good!

--- Ryddelmyst 4.27/Source/Ryddelmyst/Private/MonsterAI.cpp
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Private/MonsterAI.cpp
@@ -10,19 +10,21 @@ AMonsterAI::AMonsterAI()
 
 void AMonsterAI::BeginPlay()
 {
-    /* todo: let's just keep things simple for the BP approach until that works, then we can monkey with customization
+    Super::BeginPlay();
     GetWorldTimerManager().SetTimer(TrackingTimerHandle, this, &AMonsterAI::TrackPlayer, TrackingPeriod, true);
-    */
-   Super::BeginPlay();
+   
 }
 
 void AMonsterAI::TrackPlayer()
 {
     UE_LOG(LogTemp, Warning, TEXT("TrackPlayer; refreshing player track behavior"));
+    AActor* PursuedActor = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+    // turn towards player 1
+    FRotator LookAtRotation = (PursuedActor->GetActorLocation() - GetCharacter()->GetActorLocation()).Rotation();
+    GetCharacter()->SetActorRotation(LookAtRotation);
     // pursue player 1
-    /* todo: not working for some reason; try the BP node approach and see if that works
     MoveToActor(
-        UGameplayStatics::GetPlayerCharacter(GetWorld(), 0),
+        PursuedActor,
         100.f,
         true,
         true,
@@ -30,5 +32,4 @@ void AMonsterAI::TrackPlayer()
         nullptr,
         true
     );
-    */
 }
