1109373b4ac0642a18272ee52779fd83ccaac2bf
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-07-11 20:48:20 -0400

    added spooky teleport

--- Ryddelmyst 4.27/Source/Ryddelmyst/Private/MonsterAI.cpp
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Private/MonsterAI.cpp
@@ -1,9 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Kismet/GameplayStatics.h"
+#include "Math/UnrealMathUtility.h"
 #include "MonsterAI.h"
 
-AMonsterAI::AMonsterAI()
+AMonsterAI::AMonsterAI() 
+:
+TeleportOffset(100.f, 0.f, 0.f)
 {
     PrimaryActorTick.bCanEverTick = true;
 }
@@ -12,7 +15,7 @@ void AMonsterAI::BeginPlay()
 {
     Super::BeginPlay();
     GetWorldTimerManager().SetTimer(TrackingTimerHandle, this, &AMonsterAI::TrackPlayer, TrackingPeriod, true);
-   
+    GetWorldTimerManager().SetTimer(TeleportTimerHandle, this, &AMonsterAI::TeleportNearPlayer, TeleportPeriod, true);
 }
 
 void AMonsterAI::TrackPlayer()
@@ -32,4 +35,15 @@ void AMonsterAI::TrackPlayer()
         nullptr,
         true
     );
+}
+
+void AMonsterAI::TeleportNearPlayer()
+{
+    // generate random angle in degrees for the teleport offset vector to be rotated by; this will cause the treant to approach the player from different directions after teleporting, and by design occasionally show up in the woodwork just to jumpscare 
+    float RandAngle = FMath::RandRange(0.f, 360.f);
+    FRotator OffsetRot = FRotator(0.f, RandAngle, 0.f);
+    FVector ScaryOffset = OffsetRot.RotateVector(TeleportOffset);
+    AActor* PursuedActor = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+    GetCharacter()->SetActorLocation(PursuedActor->GetActorLocation() + ScaryOffset);
+    // todo: play a creepy noise when he teleports
 }
