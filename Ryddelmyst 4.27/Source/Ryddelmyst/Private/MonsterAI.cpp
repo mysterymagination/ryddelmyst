aae95cea350496db63b1c51406192c0296efd634
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-07-28 19:36:24 -0400

    playing with issue #49

--- Ryddelmyst 4.27/Source/Ryddelmyst/Private/MonsterAI.cpp
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Private/MonsterAI.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "MonsterAI.h"
 #include "Kismet/GameplayStatics.h"
 #include "Math/UnrealMathUtility.h"
-#include "MonsterAI.h"
 
 AMonsterAI::AMonsterAI() 
 :
