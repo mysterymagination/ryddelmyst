85f4a1af05d2ac0dc26db9416d2a7ceac3927454
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-08-12 23:25:24 -0400

    I intended to make hitboxes and instead I'm building bodies... it's aliiiiive!

---
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Private/HitHandlerComponent.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HitHandlerComponent.h"
+
+
+// Sets default values for this component's properties
+UHitHandlerComponent::UHitHandlerComponent()
+{
+	
+}
+
+
+// Called when the game starts
+void UHitHandlerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
---
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Public/ExtremityUnit.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnatomyUnit.h"
+#include "ExtremityUnit.generated.h"
+
+/**
+ * A unit of anatomy that takes less damage than the core or head, but which may be crucial to an organism's locomotion or combat prowess;
+ * damage to this unit may debilitate one or more of its stats.
+ */
+UCLASS()
+class RYDDELMYST_API UExtremityUnit : public UAnatomyUnit
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
