7b79f008f610fe1681eb0ac9a44857433ae4df78
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-08-15 21:51:50 -0400

    flailings with anatomical architecture

---
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Private/IDefender.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IDefender.h"
+
+
+// Add default functionality here for any IIDefender functions that are not pure virtual.
