4cfacdec4c4ec267398a8ef7d27eed0c3f095552
Jeff Creswell <Jeffrey.Creswell@envrmnt.com>
2022-08-15 22:00:14 -0400

    flailings with anatomical architecture

--- Ryddelmyst 4.27/Source/Ryddelmyst/Public/IDefender.h
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Public/IDefender.h
@@ -25,8 +25,35 @@ public:
 	/**
 	 * @brief Calculates the final damage received from an incoming attack
 	 * @param BaseDamage the damage the attack is attempting to deliver before armor, resistances, weaknesses etc. are taken into account
+	 * @param DamageTypes the types of damage the incoming attack deals
 	 */
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Combat")
-	float CalculateDamageRx(float BaseDamage, TSubclassOf<UDamageType> DamageType);
+	float CalculateDamageRx(float BaseDamage, TArray<TSubclassOf<UDamageType>> DamageTypes);
+	/**
+	 * @return an array of damage types that this defending object resists
+	 * 
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Combat")
+	TArray<TSubclassOf<UDamageType>> GetResistances();
+	/**
+	 * @return the scaling factor applied to resisted damage types, e.g. 0.5f reduces resisted damage by half
+	 * 
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Combat")
+	float GetResistanceFactor();
+	/**
+	 * @return an array of damage types to which this defending object is vulnerable
+	 * 
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Combat")
+	TArray<TSubclassOf<UDamageType>> GetVulnerabilities();
+	/**
+	 * @return the scaling factor applied to vulnerable damage types, e.g. 1.5f increases vulnerable damage to 150%
+	 * 
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Combat")
+	float GetVulnerabilityFactor();
+
+
 	
 };
