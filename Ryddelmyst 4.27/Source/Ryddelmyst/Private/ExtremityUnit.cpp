52ee4158317cbc293782567be9e680a3e5e5c028
mysterymagination <creswel2@gmail.com>
2022-08-22 21:48:06 -0400

    working on debilitate, the best of the megaten spells!

--- Ryddelmyst 4.27/Source/Ryddelmyst/Private/ExtremityUnit.cpp
+++ Ryddelmyst 4.27/Source/Ryddelmyst/Private/ExtremityUnit.cpp
@@ -1,8 +1,36 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "BattleStats.h"
 #include "ExtremityUnit.h"
 
-
+void UExtremityUnit::Debilitate(AActor* BattleStatsBearer)
+{
+	// todo: need to cache the old stat value and set a clock to restore it after debilitate period expires
+	if (!IsDebilitated)
+	{
+		if (BattleStatsBearer->GetClass()->ImplementsInterface(UBattleStatsBearer::StaticClass()))
+		{
+			UBattleStats* BattleStats = IBattleStatsBearer::Execute_GetStats(BattleStatsBearer);
+			if (IsUsedForAttack)
+			{
+				BattleStats->Attack *= DebilitateScaleFactor;
+			}
+			if (IsUsedForLocomotion)
+			{
+				BattleStats->Speed *= DebilitateScaleFactor;
+				IBattleStatsBearer::Execute_UpdateSpeed(BattleStatsBearer);
+			}
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Debilitate; the input AActor %s does not implement IBattleStatsBearer so we cannot modify stats"), *BattleStatsBearer->GetName());
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Debilitate; AActor %s is already suffering stat drop from a wound in %s!  Aim someplace else, yeesh."), *BattleStatsBearer->GetName(), *FormName);
+	}
+}
 
 
