8d6359606282c458d647a0ca8ea258fa8a7f50a9
mysterymagination <creswel2@gmail.com>
2022-11-28 21:20:28 -0500

    finally getting back to issue #45

---
+++ Ryddelmyst 5.0/Source/Ryddelmyst/Public/Claw.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon.h"
+#include "Claw.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RYDDELMYST_API UClaw : public UWeapon
+{
+	GENERATED_BODY()
+	
+};
