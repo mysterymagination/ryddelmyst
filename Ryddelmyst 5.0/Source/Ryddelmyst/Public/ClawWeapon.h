22d6aa8006979cd664f1cebf90fd985cbb7c5053
mysterymagination <creswel2@gmail.com>
2022-11-29 21:22:26 -0500

    mucking about with issue #45, mostly confusing things further. But starting to untangle what the heck I want going on

---
+++ Ryddelmyst 5.0/Source/Ryddelmyst/Public/ClawWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon.h"
+#include "ClawWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RYDDELMYST_API UClawWeapon : public UWeapon
+{
+	GENERATED_BODY()
+	
+};
--- Ryddelmyst 5.0/Source/Ryddelmyst/Public/IAttacker.h
+++ Ryddelmyst 5.0/Source/Ryddelmyst/Public/IAttacker.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "UObject/Interface.h"
-
 #include "IAttacker.generated.h"
 
 // This class does not need to be modified.
@@ -15,7 +14,7 @@ class UAttacker : public UInterface
 };
 
 /**
- * Interface allowing objects to provide info about how much damage their attack deals and what type of damage it is
+ * Interface allowing objects to provide info about how much damage their attack deals and what type of damage it is, and what exactly should happen when the attack connects.
  */
 class RYDDELMYST_API IAttacker
 {
@@ -39,9 +38,8 @@ public:
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Combat")
 	float CalculateDamageTx(const FString& AttackName, AActor* BattleStatsBearer);
 	/**
-	 * @param AttackName the string name of the attack for which we wish to query damage types; most attacks will only have one, but they could have multiples so we return an array
-	 * @return the types of damage dealt by this attacking object
+	 * @return the types of damage dealt by this attack
 	 */
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Combat")
-	TArray<TSubclassOf<UDamageType>> GetDamageTypes(const FString& AttackName);
+	TArray<TSubclassOf<UDamageType>> GetDamageTypes();
 };
