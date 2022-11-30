318c0114c3429c5356fcc63f473782b0e90e527b
mysterymagination <creswel2@gmail.com>
2022-11-29 21:22:26 -0500

    mucking about with issue #45, mostly confusing things further. But starting to untangle what the heck I want going on

--- Ryddelmyst 5.0/Source/Ryddelmyst/Private/Snowball.cpp
+++ Ryddelmyst 5.0/Source/Ryddelmyst/Private/Snowball.cpp
@@ -28,10 +28,10 @@ ASnowball::ASnowball()
 		CollisionComponent->SetMassOverrideInKg(NAME_None, 0.5f, true);
 
 		// On hit event handling
-		CollisionComponent->SetSimulatePhysics(true);//(false);
-		CollisionComponent->SetNotifyRigidBodyCollision(true);//(false);
-		CollisionComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);//(ECollisionEnabled::NoCollision);
-		CollisionComponent->SetCollisionProfileName("BlockAllDynamic");//("NoCollision");
+		CollisionComponent->SetSimulatePhysics(true);
+		CollisionComponent->SetNotifyRigidBodyCollision(true);
+		CollisionComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		CollisionComponent->SetCollisionProfileName("BlockAllDynamic");
 		FScriptDelegate onHitDelegate;
 		onHitDelegate.BindUFunction(this, FName("OnHit"));
 		CollisionComponent->OnComponentHit.Add(onHitDelegate);
