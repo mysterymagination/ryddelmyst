1173579504ab7109ff109784fd5eb6ffa2c8420a
mysterymagination <creswel2@gmail.com>
2022-12-14 22:33:15 -0500

    attacks and weapons and compile_commands.jsonsssss

--- Ryddelmyst 5.0/Source/Ryddelmyst/Public/ClawCapsuleComponent.h
+++ Ryddelmyst 5.0/Source/Ryddelmyst/Public/ClawCapsuleComponent.h
@@ -21,14 +21,18 @@ public:
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Combat")
 	UWeapon* GetWeapon_Implementation() { return Claw; }
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Combat")
-	void ExecuteAttack_Implementation(
+	void ExecuteAttack_Implementation
+	(
 		UPrimitiveComponent* StrikingComp,
 		AActor* StrickenActor,
 		UPrimitiveComponent* StrickenComp,
 		FVector NormalImpulse,
 		const FHitResult& HitInfoconst,
 		const FString& AttackName
-	) { Weapon->AttackMap[AttackName]->OnHit(
+	) 
+	{ 
+		Weapon->AttackMap[AttackName]->OnHit
+		(
 			StrikingComp,
 			StrickenActor,
 			StrickenComp,
