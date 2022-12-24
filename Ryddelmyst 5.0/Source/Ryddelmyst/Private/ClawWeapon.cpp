1910119e97b85fb593f8e55c66291191158fe8d2
mysterymagination <creswel2@gmail.com>
2022-12-24 00:02:02 -0500

    finally adding claw capsules for issue #45

--- Ryddelmyst 5.0/Source/Ryddelmyst/Private/ClawWeapon.cpp
+++ Ryddelmyst 5.0/Source/Ryddelmyst/Private/ClawWeapon.cpp
@@ -1,18 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "ClawSlashAttack.h"
 #include "ClawWeapon.h"
-
-//const FString ATTACK_CLAW_BASIC = "attack_claw_basic";
 
 UClawWeapon::UClawWeapon()
 {
     UE_LOG(LogTemp, Warning, TEXT("ctor clawweapon"));
-    /*
     AttackMap = 
     {
-        //ATTACK_CLAW_BASIC : CreateDefaultSubObject<UClawAttack>()
+        {UClawSlashAttack::ATTACK_NAME, CreateDefaultSubobject<UClawSlashAttack>(FName(*UClawSlashAttack::ATTACK_NAME))}
     };
-    */
 }
 
--- Ryddelmyst 5.0/Source/Ryddelmyst/Public/ExtremityUnit.h
+++ Ryddelmyst 5.0/Source/Ryddelmyst/Public/ExtremityUnit.h
@@ -10,7 +10,7 @@
  * A unit of anatomy that takes less damage than the core or head, but which may be crucial to an organism's locomotion or combat prowess;
  * damage to this unit may debilitate one or more of its stats.
  */
-UCLASS()
+UCLASS(ClassGroup = "Combat", editinlinenew, Blueprintable, BlueprintType, meta = (DisplayName = "Extremity", BlueprintSpawnableComponent))
 class RYDDELMYST_API UExtremityUnit : public UAnatomyUnit
 {
 	GENERATED_BODY()
