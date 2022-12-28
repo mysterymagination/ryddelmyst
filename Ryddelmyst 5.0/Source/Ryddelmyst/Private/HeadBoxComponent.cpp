aced3d228f82eca4ac30b6d88662b75211a8dfcf
mysterymagination <creswel2@gmail.com>
2022-12-27 22:24:54 -0500

    fleshing out defaults for body and head collisions for issue #45

--- Ryddelmyst 5.0/Source/Ryddelmyst/Private/HeadBoxComponent.cpp
+++ Ryddelmyst 5.0/Source/Ryddelmyst/Private/HeadBoxComponent.cpp
@@ -3,6 +3,14 @@
 
 #include "HeadBoxComponent.h"
 
+UAnatomyUnit* UHeadBoxComponent::GetAnatomyUnit_Implementation() 
+{
+    return HeadUnit;
+}
 
+UArmor* UHeadBoxComponent::GetArmor_Implementation()
+{
+    return Armor;
+}
 
 
--- Ryddelmyst 5.0/Source/Ryddelmyst/Public/BodyBoxComponent.h
+++ Ryddelmyst 5.0/Source/Ryddelmyst/Public/BodyBoxComponent.h
@@ -4,17 +4,26 @@
 
 #include "CoreMinimal.h"
 #include "Components/BoxComponent.h"
+#include "CoreUnit.h"
+#include "IAnatomy.h"
+#include "IDefender.h"
 #include "BodyBoxComponent.generated.h"
 
 /**
- * 
+ * Form representing the physical core of a creature
  */
-UCLASS()
-class RYDDELMYST_API UBodyBoxComponent : public UBoxComponent
+UCLASS(ClassGroup = "Collision", editinlinenew, Blueprintable, BlueprintType, meta = (DisplayName = "Body Collision", BlueprintSpawnableComponent))
+class RYDDELMYST_API UBodyBoxComponent : public UBoxComponent, public IAnatomy, public IDefender
 {
-	GENERATED_BODY()
-	
-	
-	
-	
+    GENERATED_BODY()
+    
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
+    UCoreUnit* CoreUnit;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
+    UArmor* Armor;
+
+public:
+    UAnatomyUnit* GetAnatomyUnit_Implementation() override;
+    UArmor* GetArmor_Implementation() override;
 };
