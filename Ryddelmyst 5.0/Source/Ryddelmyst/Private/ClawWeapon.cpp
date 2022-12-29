46fef0c8c54309855bcfd045069bd63e408dec7b
mysterymagination <creswel2@gmail.com>
2022-12-28 22:23:20 -0500

    fighting with the editor metadata for issue #45 so I can assign anatomy unit and armor and weapon from editor

--- Ryddelmyst 5.0/Source/Ryddelmyst/Private/ClawWeapon.cpp
+++ Ryddelmyst 5.0/Source/Ryddelmyst/Private/ClawWeapon.cpp
@@ -1,8 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "ClawSlashAttack.h"
 #include "ClawWeapon.h"
+#include "ClawSlashAttack.h"
 
 UClawWeapon::UClawWeapon()
 {
