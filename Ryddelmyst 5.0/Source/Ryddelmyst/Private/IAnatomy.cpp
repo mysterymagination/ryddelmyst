3f27eca31b20b527d203e2ee8ced5aced00201c7
mysterymagination <creswel2@gmail.com>
2023-01-10 19:23:33 -0500

    actually removing the old 5.0 folder this time, and somehow repairing BP_ReaperTreant uasset by rolling it back to an older commit

--- Ryddelmyst 5.0/Source/Ryddelmyst/Private/IAnatomy.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "IAnatomy.h"
-
-
-// Add default functionality here for any IIAnatomy functions that are not pure virtual.
