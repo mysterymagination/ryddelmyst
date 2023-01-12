51f2aef7f6b81e0f5eafb0f38fe44880b8a67c6d
mysterymagination <creswel2@gmail.com>
2023-01-12 14:51:34 -0500

    implementing boilerplate armor handling for issue #45

--- Ryddelmyst 5.1/Source/Ryddelmyst/Private/Armor.cpp
+++ Ryddelmyst 5.1/Source/Ryddelmyst/Private/Armor.cpp
@@ -2,3 +2,9 @@
 
 
 #include "Armor.h"
+
+float UArmor::CalculateDamageRx_Implementation(AActor* BattleStatsBearer, UAnatomyUnit* AnatomyCovered, float BaseDamage, const TArray<TSubclassOf<UDamageType>>& DamageTypes) 
+{
+    UBattleStats* Stats = IBattleStatsBearer::Execute_GetStats(BattleStatsBearer);
+    return BaseDamage * DamageReductionFactor * AnatomyCovered->DamageScaleFactor - Stats->StatsMap["Defense"];
+}
