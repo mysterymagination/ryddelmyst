55ac4e27bc6b1d9052cf8ac23f081df6111f774c
mysterymagination <creswel2@gmail.com>
2023-01-30 18:30:55 -0500

    mucking with snowball, creating a form and function paradigm using the IAttacker shape, Weapon, Attacks API

---
+++ Ryddelmyst 5.1/Source/Ryddelmyst/Public/MagicWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon.h"
+#include "MagicWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RYDDELMYST_API UMagicWeapon : public UWeapon
+{
+	GENERATED_BODY()
+	
+};
