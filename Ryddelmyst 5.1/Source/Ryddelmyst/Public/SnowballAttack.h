4dec375ef61bf0bc37d2b74b9b0eb0b9d707ac0b
mysterymagination <creswel2@gmail.com>
2023-01-30 18:30:55 -0500

    mucking with snowball, creating a form and function paradigm using the IAttacker shape, Weapon, Attacks API

---
+++ Ryddelmyst 5.1/Source/Ryddelmyst/Public/SnowballAttack.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Attack.h"
+#include "SnowballAttack.generated.h"
+
+/**
+ * An Attack representing the damage formula and configurable effects/damage type of the Snowball spell
+ */
+UCLASS()
+class RYDDELMYST_API USnowballAttack : public UAttack
+{
+	GENERATED_BODY()
+	// todo: need configurable damage types
+	// todo: need configurable effects
+};
