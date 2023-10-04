c812e6dd29b0f3fb9397fa76f135ad587248fc2e
mysterymagination <creswel2@gmail.com>
2023-10-03 22:22:24 -0400

    starting in on issue #76

---
+++ Ryddelmyst_5.3/Source/Ryddelmyst/Private/DataConversionUtils.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DataConversionUtils.h"
+
+static void DataConversionUtils::FX_NumberParticles(const& FVector Location, float Value)
+{
+    // todo: round the input Value to nearest int
+    // todo: convert integer value to string
+    // todo: loop through the string characters and spawn particle system at the given location plus some offset for each
+    //  e.g. '1' '2' '3' for input 123.1
+}
