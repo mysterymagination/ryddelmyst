ddae3206c9390a84c392f9f63c78cd4bb623b545
mysterymagination <creswel2@gmail.com>
2023-10-14 22:19:46 -0400

    I think I finally cobbled together the necessary docs to emulate that damn spawn niagara system like its nuthin BP node

---
+++ Ryddelmyst_5.3/Source/Ryddelmyst/Private/DataViz.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DataViz.h"
+#include "NiagaraCommon.h"
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+
+void DataViz::FX_NumberParticles(UObject* World, const FVector& Location, const FRotator& Rotation, const FVector& Scale, float Value)
+{
+    // todo: round the input Value to nearest int
+    // todo: convert integer value to string
+    // todo: loop through the string characters 
+    // todo: spawn particle system at the given location plus some offset for each
+    //  e.g. '1' '2' '3' for input 123.1
+    UNiagaraSystem* FloatingNumbersFX_System = LoadObject<UNiagaraSystem>
+    (
+        nullptr, 
+        TEXT("/Game/Ryddelmyst_Assets/Particles/P_FloatingNumbersFX.P_FloatingNumbersFX"), 
+        nullptr, 
+        LOAD_None, 
+        nullptr
+    );
+    // todo: how do we provide inputs to the niagara system?
+    SpawnSystemAtLocation
+    (
+        World,
+        FloatingNumbersFX_System,
+        Location,
+        Rotation,
+        true,
+        true,
+        ENCPoolMethod::None,
+        true
+    );
+}
--- Ryddelmyst_5.3/Source/Ryddelmyst/Public/DataViz.h
+++ Ryddelmyst_5.3/Source/Ryddelmyst/Public/DataViz.h
@@ -7,13 +7,14 @@
 /**
  * Util functions to convert and display data e.g. display a numeric value as a particle effect
  */
-class RYDDELMYST_API DataConversionUtils
+class RYDDELMYST_API DataViz
 {
 public:
 	/**
-	 * Generates a particle effect using the P_FloatingNumbersFX system at the given location and representing the given value
+	 * Generates a particle effect using the P_FloatingNumbersFX Niagara system at the given location and representing the given value
+	 * @param World the world context object we're spawning the particles into
 	 * @param Location a 3D vector showing where the particle FX should appear in 3D space
 	 * @param Value a float that will be rounded to the nearest integer and displayed as a particle FX
 	 */
-	static void FX_NumberParticles(const FVector& Location, float Value);
+	static void FX_NumberParticles(UObject* World, const FVector& Location, const FRotator& Rotation, const FVector& Scale, float Value);
 };
