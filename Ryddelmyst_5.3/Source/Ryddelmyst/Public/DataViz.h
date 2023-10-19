193a56a5b9745c3242ff105a7fcec18596142288
mysterymagination <creswel2@gmail.com>
2023-10-18 21:34:17 -0400

    updating source only for the damage number spawn since I apparently overran my LFS bandwidth

--- Ryddelmyst_5.3/Source/Ryddelmyst/Public/DataViz.h
+++ Ryddelmyst_5.3/Source/Ryddelmyst/Public/DataViz.h
@@ -15,6 +15,8 @@ public:
 	 * @param World the world context object we're spawning the particles into
 	 * @param Location a 3D vector showing where the particle FX should appear in 3D space
 	 * @param Value a float that will be rounded to the nearest integer and displayed as a particle FX
+	 * @param DamageAmp float representing the percentage of potential damage that became actual damage e.g. how much of damage TX made it through damage RX to ApplyDamage() in UAttack::OnHit().
+	 * @param IsCrit true if the random aspect of the damage roll was 90+% of the possible rolled value, false otherwise.
 	 */
-	static void FX_NumberParticles(UObject* World, const FVector& Location, const FRotator& Rotation, const FVector& Scale, float Value);
+	static void FX_NumberParticles(UObject* World, const FVector& Location, const FRotator& Rotation, const FVector& Scale, float Value, float DamageAmp, bool IsCrit);
 };
