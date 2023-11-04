e524a6ea9b6d4af0f3bac660387055221c4dd83c
mysterymagination <creswel2@gmail.com>
2023-11-04 00:43:59 -0400

    working on treant handledeath strat

--- Ryddelmyst_5.3/Source/Ryddelmyst/Private/BurnedStatusEffect.cpp
+++ Ryddelmyst_5.3/Source/Ryddelmyst/Private/BurnedStatusEffect.cpp
@@ -12,27 +12,36 @@ UBurnedStatusEffect::UBurnedStatusEffect()
 
 void UBurnedStatusEffect::OnEffectApplied(AActor* EffectedActor)
 {
-    // start a timer that runs a function to apply fire damage at the given period
-    FTimerDelegate BurnTimerDelegate;
-	BurnTimerDelegate.BindUFunction(this, FName("Burn"), EffectedActor);
-    EffectedActor->GetWorldTimerManager().SetTimer(BurnTimerHandle, BurnTimerDelegate, BurnPeriod, true);
-    // start a timer that will stop the burn damage applbication timer when the burn duration expires
-    FTimerDelegate BurnCancelTimerDelegate;
-	BurnCancelTimerDelegate.BindUFunction(this, FName("OnEffectRemoved"), EffectedActor);
-    EffectedActor->GetWorldTimerManager().SetTimer(TotalBurnTimerHandle, BurnCancelTimerDelegate, BurnDuration, false);
+    if (IsValid(EffectedActor))
+    {
+        // start a timer that runs a function to apply fire damage at the given period
+        FTimerDelegate BurnTimerDelegate;
+        BurnTimerDelegate.BindUFunction(this, FName("Burn"), EffectedActor);
+        EffectedActor->GetWorldTimerManager().SetTimer(BurnTimerHandle, BurnTimerDelegate, BurnPeriod, true);
+        // start a timer that will stop the burn damage applbication timer when the burn duration expires
+        FTimerDelegate BurnCancelTimerDelegate;
+        BurnCancelTimerDelegate.BindUFunction(this, FName("OnEffectRemoved"), EffectedActor);
+        EffectedActor->GetWorldTimerManager().SetTimer(TotalBurnTimerHandle, BurnCancelTimerDelegate, BurnDuration, false);
+    }
 }
 
 void UBurnedStatusEffect::OnEffectRemoved(AActor* EffectedActor)
 {
-    // cancel the burn timer
-    EffectedActor->GetWorldTimerManager().ClearTimer(BurnTimerHandle);
+    if (IsValid(EffectedActor))
+    {
+        // cancel the burn timer
+        EffectedActor->GetWorldTimerManager().ClearTimer(BurnTimerHandle);
+    }
 }
 
 void UBurnedStatusEffect::Burn(AActor* EffectedActor)
 {
-    // todo: surround Actor with fire particle effect
-    UE_LOG(LogTemp, Log, TEXT("Burn; damaging %s for %f fire damage from their burn"), *EffectedActor->GetName(), BurnDamage);
-    UGameplayStatics::ApplyDamage(EffectedActor, BurnDamage, nullptr, nullptr, UFireDamageType::StaticClass());
+    if (IsValid(EffectedActor))
+    {
+        // todo: surround Actor with fire particle effect
+        UE_LOG(LogTemp, Log, TEXT("Burn; damaging %s for %f fire damage from their burn"), *EffectedActor->GetName(), BurnDamage);
+        UGameplayStatics::ApplyDamage(EffectedActor, BurnDamage, nullptr, nullptr, UFireDamageType::StaticClass());
+    }
 }
 
 
