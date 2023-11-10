9ce02fff4ed5c06aa0e0019efe160d038af95d80
mysterymagination <creswel2@gmail.com>
2023-11-09 23:13:59 -0500

    starting in on general spell bullets and blaster bolts in particular

---
+++ Ryddelmyst_5.3/Source/Ryddelmyst/Public/SpellBullet.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NiagaraSystem.h"
+#include "HitBoxerComponent.h"
+#include "Components/ShapeComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "SpellProjectile.generated.h"
+
+UCLASS()
+class RYDDELMYST_API ASpellBullet : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpellBullet();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Collision)
+	UHitBoxerComponent* HitBoxer;
+	// Projectile particle FX
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Projectile)
+	UNiagaraSystem* BulletParticles;
+	// Projectile material
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Projectile)
+	UMaterialInstanceDynamic* BulletMaterial;
+	// Projectile mesh
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Projectile)
+	UStaticMeshComponent* BulletMesh;
+	// Collision shape component.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Projectile)
+	UShapeComponent* BulletShape;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Projectile)
+	float Mass = 0.5f;
+private:
+	// Projectile movement component.
+	UPROPERTY(VisibleAnywhere, Category = Movement)
+	UProjectileMovementComponent* BulletMovement;
+};
