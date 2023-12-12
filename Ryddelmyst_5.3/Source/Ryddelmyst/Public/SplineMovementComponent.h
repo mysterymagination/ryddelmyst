3eb5b3751032a2452c3a34c96184c8736e2ba532
mysterymagination <creswel2@gmail.com>
2023-12-11 22:12:50 -0500

    added template for spline movement component

---
+++ Ryddelmyst_5.3/Source/Ryddelmyst/Public/SplineMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/MovementComponent.h"
+#include "SplineMovementComponent.generated.h"
+
+/**
+ * Moves an Actor's root SceneComponent along a spline path; the path may be prefabricated and provided or generated at runtime with parameters.
+ */
+UCLASS()
+class RYDDELMYST_API USplineMovementComponent : public UMovementComponent
+{
+	GENERATED_BODY()
+	
+};
