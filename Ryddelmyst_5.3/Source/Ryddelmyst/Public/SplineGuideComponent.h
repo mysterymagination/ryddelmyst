907a7c0b9f02b221b77a68a438848f04e62a6bea
mysterymagination <creswel2@gmail.com>
2023-12-17 03:55:03 -0500

    some arch based on movementcomponent

--- Ryddelmyst_5.3/Source/Ryddelmyst/Public/SplineGuideComponent.h
+++ Ryddelmyst_5.3/Source/Ryddelmyst/Public/SplineGuideComponent.h
@@ -4,16 +4,23 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/SceneComponent.h"
+#include "Components/SplineComponent.h"
 #include "SplineGuideComponent.generated.h"
 
 /**
  * Moves an array of Actor bullets' root SceneComponents along a spline path; the path may be prefabricated and provided or generated at runtime with parameters.
- * We'll have a single spline with this component directing bullets' movement along it; we'll set up a transform attachment between the spline and the spawned bullet actors and then apply AddMovementInput() with a given speed and direction derived by using a point placed in front of the bullet 
- * and sampling for the nearest spline point to it with FindLocationClosestToWorldLocation() so the bullet is continously making its way along the spline.
+ * We'll have a single spline with this component directing bullets' movement along it; we'll set up a transform attachment between the spline and the spawned bullet actors and then apply AddMovementInput() with a given speed and direction derived by using a point placed in front of the bullet and sampling for the nearest spline point to it with FindLocationClosestToWorldLocation() so the bullet is continously making its way along the spline.
  */
 UCLASS()
 class RYDDELMYST_API USplineGuideComponent : public USceneComponent
 {
 	GENERATED_BODY()
+
+public:
+	/**
+	 * Pointer to the USplineComponent that this component guides actors along. 
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Navigation")
+	USplineComponent* Spline;
 	
 };
