72496d7320c7ced94d1f1488571fd1d5bbd7a49a
mysterymagination <creswel2@gmail.com>
2024-02-22 23:20:14 -0500

    adding flame bead bullet for bullet spray

---
+++ Ryddelmyst_5.3/Source/Ryddelmyst/Public/FlameBeadBullet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SpellBullet.h"
+#include "FlameBeadBullet.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = "Combat", Blueprintable, BlueprintType, meta = (DisplayName = "SpellBullet", BlueprintSpawnableComponent))
+class RYDDELMYST_API AFlameBeadBullet : public ASpellBullet
+{
+	GENERATED_BODY()
+	
+};
