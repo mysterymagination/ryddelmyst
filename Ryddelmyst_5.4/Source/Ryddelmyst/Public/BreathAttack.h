779ed658db5eca0bb057d07849586f8429d2407c
mysterymagination <creswel2@gmail.com>
2024-07-14 03:17:33 -0400

    removing lava block nav mod on wood egg destruction

---
+++ Ryddelmyst_5.4/Source/Ryddelmyst/Public/BreathAttack.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Attack.h"
+#include "BreathAttack.generated.h"
+
+/**
+ * An attack that deals damage based on one or more of a creature's stats,
+ * usually HP, and ignores any defenses of the target.
+ */
+UCLASS()
+class RYDDELMYST_API UBreathAttack : public UAttack
+{
+	GENERATED_BODY()
+	
+};
---
+++ Ryddelmyst_5.4/Source/Ryddelmyst/Public/WoodfirebreathAttack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BreathAttack.h"
+#include "WoodfirebreathAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RYDDELMYST_API UWoodfirebreathAttack : public UBreathAttack
+{
+	GENERATED_BODY()
+	
+};
