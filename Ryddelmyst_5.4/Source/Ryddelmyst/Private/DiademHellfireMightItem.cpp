ab1acd590bbed49b09b87d586fc77035c8841cd5
Jeffrey Creswell <jcreswell@meta.com>
2024-07-24 18:28:04 -0400

    tweaking firesnowball so knockback is mildly phsyics based instead of stupid teleport that gets things stuck in hedges all the itme

--- Ryddelmyst_5.4/Source/Ryddelmyst/Private/DiademHellfireMightItem.cpp
+++ Ryddelmyst_5.4/Source/Ryddelmyst/Private/DiademHellfireMightItem.cpp
@@ -26,10 +26,27 @@ void UDiademHellfireMightItem::OnEquip_Implementation(AActor* EquippedActor)
 			UE_LOG(LogTemp, Warning, TEXT("Metamagic Fire lambda; onhit effect"));
 			if (TargetActor->IsRootComponentMovable())
 			{
+				/* todo: for some reason turning physics on for the treant hitbox components causes snowballs not to register hits, and if I enable on the collision cyl he goes flying like a cartoon balloon unless I have it ignore HitBox collision type. That can be fine, but then he's rolling around all over the place after teleport for some reason.
+				TArray<UPrimitiveComponent*> OutPrims;
+				TargetActor->GetComponents<UPrimitiveComponent>(OutPrims, true);
+				for (auto Prim : OutPrims)
+				{
+					// todo: first check if physics is enabled for comp
+					Prim->AddImpulse(HitResult.ImpactNormal * 1500.f, NAME_None, true);
+				}
+				*/
+				/* reliable but silly simple teleport back approach
 				FVector PreviousActorLocation = TargetActor->GetActorLocation();
 				TargetActor->SetActorLocation(TargetActor->GetActorLocation() + HitResult.ImpactNormal * -250.f);
 				UE_LOG(LogTemp, Warning, TEXT("Metamagic Fire lambda; just moved %s from %s to %s via impact normal %s times -250"), 
 					*TargetActor->GetName(), *PreviousActorLocation.ToString(), *TargetActor->GetActorLocation().ToString(), *HitResult.ImpactNormal.ToString()); 
+				*/
+				if (ACharacter* Character = Cast<ACharacter>(TargetActor))
+				{
+					FVector NormalImpulseVector = HitResult.ImpactNormal;
+					NormalImpulseVector.Normalize(0);
+					Character->LaunchCharacter(5000.f * -NormalImpulseVector, false, false);
+				}
 			}
 			else
 			{
