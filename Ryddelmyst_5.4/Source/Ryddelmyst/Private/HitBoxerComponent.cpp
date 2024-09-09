56856d56fc3ea174f1b580b46b2c1d1425fdbc71
Jeffrey Creswell <jcreswell@meta.com>
2024-09-09 14:49:18 -0400

    lady exclaiming sort of thing

--- Ryddelmyst_5.4/Source/Ryddelmyst/Private/HitBoxerComponent.cpp
+++ Ryddelmyst_5.4/Source/Ryddelmyst/Private/HitBoxerComponent.cpp
@@ -20,7 +20,6 @@ void UHitBoxerComponent::OnHit(UPrimitiveComponent* StrikingComp, AActor* Strick
 			UE_LOG(LogTemp, Warning, TEXT("HitBoxer::OnHit; %s's striking comp %s weapon %s is running attack %s on %s's shapely %s"),
 				*StrikingComp->GetOwner()->GetName(), *StrikingComp->GetName(), (Weapon ? *Weapon->GetName() : TEXT("null weapon")), *(*Attack_Check)->AttackName, *StrickenActor->GetName(), *StrickenComp->GetName());
 			(*Attack_Check)->OnHit_Implementation(IAttacker::Execute_GetBattleStats(StrikingComp), StrikingComp, StrickenActor, StrickenComp, NormalImpulse, HitInfo);
-
 		}
 		else
 		{
