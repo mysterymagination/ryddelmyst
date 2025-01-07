bdc6196fb49225a9e7d9dad4c51ac54335d0b6fb
Jeffrey Creswell <jcreswell@meta.com>
2025-01-06 22:00:19 -0500

    ok good nuff

--- Ryddelmyst_5.4/Source/Ryddelmyst/Private/RyddelmystEventManager.cpp
+++ Ryddelmyst_5.4/Source/Ryddelmyst/Private/RyddelmystEventManager.cpp
@@ -8,13 +8,13 @@
 
 void URyddelmystEventManager::ScheduleQuestCompletionEvent(UGameInstance* GameInstance, float DelaySeconds, FString CompletionContext)
 {
-	GameInstance->GetTimerManager().SetTimer(ScheduledEventTimerHandle, [&]() {//[GameInstance, CompletionContext]() {
-        //UE_LOG(LogTemp, Warning, TEXT("test quest completion broadcast with test string %s"), *CompletionContext);
+	GameInstance->GetTimerManager().SetTimer(ScheduledEventTimerHandle, [GameInstance, CompletionContext]() {
+        UE_LOG(LogTemp, Warning, TEXT("test quest completion broadcast with test string %s"), *CompletionContext);
 		auto* RyddelmystGameInstance = Cast<URyddelmystGameInstance>(GameInstance);
 		UE_LOG(LogTemp, Warning, TEXT("test quest completion broadcast; gameinstance says %p"), RyddelmystGameInstance);
         auto* EventManager = RyddelmystGameInstance->GetEventManager();
 		UE_LOG(LogTemp, Warning, TEXT("test quest completion broadcast; eventmanager says %p"), EventManager);
 		UE_LOG(LogTemp, Warning, TEXT("test quest completion broadcast; questcompletionevent address says %p"), &EventManager->QuestCompletionEvent);
-		EventManager->QuestCompletionEvent.Broadcast(TEXT("testing quetsing"));//Broadcast(CompletionContext);
+		EventManager->QuestCompletionEvent.Broadcast(CompletionContext);
     }, DelaySeconds, false);
 }
