ad7363c87f566f941aa58661e7cc0ac73a0e710d
Jeffrey Creswell <jcreswell@meta.com>
2025-01-13 18:04:18 -0500

    fixing quest log crash and removing default pawn class to fix splash screen

--- Ryddelmyst_5.4/Source/Ryddelmyst/Public/ConversationStarter.h
+++ Ryddelmyst_5.4/Source/Ryddelmyst/Public/ConversationStarter.h
@@ -186,13 +186,14 @@ public:
 #endif // PLATFORM_HAS_BSD_TIME 
 	/**
 	 * @brief Returns the JSON string content of the conversation script that should be used.
-	 * @param ConvoTx - the name of the Actor who instigated the conversation.
-	 * @param ConvoRx - the name of the Actor who ConvoTx started a conversation with.
-	 * @param ClosestBone - indicates where the player was looking when initiating the convo.
-	 * @param GameState - handle to the current game state so that it can modify which conversation script is returned.
+	 * @param _ConvoTx - the name of the Actor who instigated the conversation.
+	 * @param _ConvoRx - the name of the Actor who ConvoTx started a conversation with.
+	 * @param _ClosestBone - indicates where the player was looking when initiating the convo.
+	 * @param _GameState - handle to the current game state so that it can modify which conversation script is returned.
 	 * @return the best fit conversation JSON script contents as an FString.
 	 */
-	void Init(const FString& ConvoTx, const FString& ConvoRx, FName ClosestBone, ARyddelmystGameState* GameState);
+	UFUNCTION(BlueprintCallable, Category = "Dialogue")
+	void Init(const FString& _ConvoTx, const FString& _ConvoRx, FName _ClosestBone, ARyddelmystGameState* _GameState);
 
 	/**
 	 * Uses the conversing character identities and game state set in Init() to determine the correct conversation script.
